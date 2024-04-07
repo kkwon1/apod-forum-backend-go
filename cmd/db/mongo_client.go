d8b9f56ea0d234ca9f3b5f14d1b798e28b8ed72e
Kevin Kwon <kevinkwon1995@gmail.com>
2024-04-06 20:48:19 -0700

    Use updated functions for mongo client

--- cmd/db/mongo_client.go
+++ cmd/db/mongo_client.go
@@ -13,13 +13,8 @@ type MongoDBClient struct {
 }
 
 func NewMongoDBClient(connectionString string) (*MongoDBClient, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
-	if err != nil {
-		return nil, err
-	}
-
 	ctx := context.Background()
-	err = client.Connect(ctx)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
 	if err != nil {
 		return nil, err
 	}
@@ -34,11 +29,3 @@ func (mc *MongoDBClient) Close() {
 func (mc *MongoDBClient) GetDatabase(databaseName string) *mongo.Database {
 	return mc.client.Database(databaseName)
 }
-
-// Example usage:
-// dbClient, err := mongodb.NewMongoDBClient("mongodb://localhost:27017")
-// if err != nil {
-//     log.Fatal(err)
-// }
-// defer dbClient.Close()
-// db := dbClient.GetDatabase("mydatabase")
